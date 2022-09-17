45be208df33893f526b78a65e953eda84d18ead3
seeratalishah <seeratalyshah@gmail.com>
2022-09-17 16:29:32 +0500

    added state variable for search city input

--- src/components/SearchCity.js
+++ src/components/SearchCity.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function SearchCity() {
+  const [city, setCity] = useState('');
   return (
     <form action="">
       <label htmlFor="city">City: </label>
-      <input type="text" id="city" placeholder="City Name" required />
+      <input type="text" id="city" placeholder="City Name" value={city} onChange={(e)=>setCity(e.target.value)} required />
       <button type="submit" >search weather</button>
     </form> 
   )
